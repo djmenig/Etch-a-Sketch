43a0174d5565511ce52ac533d19aca8dd56c83fe
DJMenig <djmenig@outlook.com>
2022-09-03 15:12:34 -0500

    Create clearGrid function and added it to changeGridSize function.

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ function createGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`; //stylizes #grid columns according to the size variable
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`; //stylizes #grid rows according to the size variable
 
-    for(i = 0; i < gridSizeSquared; i++) { //creates the drawing effect with the mouse pointer by "filling" the grid with squares of the background color
+    //creates the drawing effect with the mouse pointer by "filling" the grid with squares of the background color
+    for(i = 0; i < gridSizeSquared; i++) {
         let square = document.createElement('div');
+        square.classList.add('square');
         grid.insertAdjacentElement('beforeend', square);
         square.addEventListener('mouseover', (event) => {
             square.style.backgroundColor = 'black';
@@ -16,20 +18,18 @@ function createGrid(size) {
 
 createGrid(16);
 
-let userInput
 function clearGrid() {
-    let squares = document.querySelector('#grid').children;
-    for(i = 0; i < userInput*userInput; i++) {
-    squares.style.backgroundColor = 'white';
-    }
-    console.log(userInput);
+    let squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.removeAttribute('style');
+    })
 }
 
 function changeGridSize() {
     userInput = prompt("How many squares per side for the new grid? (2-100)");
     if(userInput >=2 && userInput <=100) {
-        createGrid(userInput);
         clearGrid();
+        createGrid(userInput);
     } else {
         alert("Please enter a value between 2-100.");
     }
